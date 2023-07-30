eb3b2104c3db38fa6ee4d53e4674de507426b429
MexL <madiajijah7@gmail.com>
2023-07-30 23:13:00 +0800

    not found page and plans page

--- src/pages/notFound/NotFound.tsx
+++ src/pages/notFound/NotFound.tsx
@@ -1,7 +1,16 @@
-import React from "react";
+import { Link } from "react-router-dom";
 
 function NotFound() {
-  return <div>NotFound</div>;
+  return (
+    <section>
+      <div className="container notfound__container">
+        <h2>Page Not Found</h2>
+        <Link to="/" className="btn">
+          Go Back to Home Page
+        </Link>
+      </div>
+    </section>
+  );
 }
 
 export default NotFound;
--- src/pages/plans/Plans.tsx
+++ src/pages/plans/Plans.tsx
@@ -1,7 +1,43 @@
-import React from "react";
+import { plans } from "../../data";
+import Header from "../../components/Header";
+import Card from "../../UI/Card";
+import HeaderImage from "../../images/header_bg_4.jpg";
+import "./plans.css";
 
 function Plans() {
-  return <div>Plans</div>;
+  return (
+    <>
+      <Header title="Membership Plans" image={HeaderImage}>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit corrupti
+        in magni! Modi minus corrupti itaque quasi recusandae nesciunt eius.
+        Quae assumenda temporibus magnam amet dolor sapiente distinctio, quia
+        ad?
+      </Header>
+      <section className="planss">
+        <div className="container plans__container">
+          {plans.map(({ id, name, desc, price, features }) => {
+            return (
+              <Card key={id} className="plan">
+                <h3>{name}</h3>
+                <small>{desc}</small>
+                <h1>{`$${price}`}</h1>
+                <h2>/mo</h2>
+                <h4>Features</h4>
+                {features.map(({ feature, available }, index) => {
+                  return (
+                    <p key={index} className={!available ? "disabled" : ""}>
+                      {feature}
+                    </p>
+                  );
+                })}
+                <button className="btn lg">Choose Plan</button>
+              </Card>
+            );
+          })}
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default Plans;
